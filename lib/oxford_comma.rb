569d82ba96fb5ace851ae5f8f478461bb2efd9b0
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-08 01:14:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   else
-    array.last = "and " << array.last
+    last = array.pop = "and " << array.last
     array.join(", ")
   end
 end
